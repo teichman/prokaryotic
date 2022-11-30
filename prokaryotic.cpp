bd7947c0dc14b82fee063dae3225643dd87017bb
Alex Teichman <teichman@cs.stanford.edu>
2022-11-30 10:26:30 -0800

    Entity->MoleculeType, adding mass, adding ctors for leaf node vs composite Molecule.

--- prokaryotic.cpp
+++ prokaryotic.cpp
@@ -1,5 +1,7 @@
+#include <cassert>
 #include <sstream>
 #include <vector>
+#include <map>
 #include <iostream>
 #include <memory>
 #include <string>
@@ -17,7 +19,7 @@ public:
   // Ugh maybe I should go back to python.
   // https://stackoverflow.com/questions/37803255/how-to-call-base-classs-function-that-is-not-defined-in-derived-class
   virtual std::string _str() const = 0;
-  virtual std::string str(const std::string& prefix="") const
+  std::string str(const std::string& prefix="") const
   {
     std::istringstream iss(_str());
     std::ostringstream oss;
@@ -31,43 +33,62 @@ public:
   }
 };
 
-class Entity : public Printable
+// Superclass for proteins and small molecules
+class MoleculeType : public Printable
 {
 public:
-  typedef std::shared_ptr<Entity> Ptr;
-  typedef std::shared_ptr<const Entity> ConstPtr;
+  typedef std::shared_ptr<MoleculeType> Ptr;
+  typedef std::shared_ptr<const MoleculeType> ConstPtr;
   
   int id_;
   std::string name_;
-  std::string emoji_;
+  std::string symbol_;
+  double daltons_;
+  std::vector<MoleculeType::Ptr> constituents_;
 
-  Entity(const std::string& name, const std::string& emoji);
+  MoleculeType(const std::string& name, const std::string& symbol, double daltons);
+  MoleculeType(const std::string& name, const std::string& symbol, const std::vector<MoleculeType::Ptr>& constituents);
   virtual std::string _str() const;
-  static size_t numEntities() { return num_entities_; }
+  static size_t numMoleculeTypes() { return num_molecule_types_; }
   
 private:
-  static size_t num_entities_;
+  static size_t num_molecule_types_;
 };
 
-size_t Entity::num_entities_ = 0;
+size_t MoleculeType::num_molecule_types_ = 0;
 
-Entity::Entity(const std::string& name,
-               const std::string& emoji) :
-  id_(num_entities_),
+MoleculeType::MoleculeType(const std::string& name,
+                           const std::string& symbol,
+                           double daltons) :
+  id_(num_molecule_types_),
   name_(name),
-  emoji_(emoji)
+  symbol_(symbol),
+  daltons_(daltons)
 {
-  num_entities_ += 1;
-  cout << "num_entities_: " << num_entities_ << endl;
-  cout << "Created Entity with id " << id_ << endl;
+  num_molecule_types_ += 1;
 }
 
-std::string Entity::_str() const
+MoleculeType::MoleculeType(const std::string& name, const std::string& symbol,
+                           const std::vector<MoleculeType::Ptr>& constituents) :
+  id_(num_molecule_types_),
+  name_(name),
+  symbol_(symbol),
+  daltons_(0),
+  constituents_(constituents)
+{
+  num_molecule_types_ += 1;
+  daltons_ = 0;
+  for (auto mt : constituents_)
+    daltons_ += mt->daltons_;
+}
+
+std::string MoleculeType::_str() const
 {
   std::ostringstream oss;
-  oss << "Entity \"" << name_ << "\"" << endl;
-  oss << "id_: " << id_ << endl;
-  oss << "emoji_: " << emoji_ << endl;
+  oss << "MoleculeType \"" << name_ << "\"" << endl;
+  oss << "  id_: " << id_ << endl;
+  oss << "  symbol_: " << symbol_ << endl;
+  oss << "  daltons_: " << daltons_ << endl;
   return oss.str();
 }
 
@@ -77,16 +98,16 @@ public:
   typedef std::shared_ptr<Biome> Ptr;
   typedef std::shared_ptr<const Biome> ConstPtr;
 
-  std::vector<size_t> flux_;  // Aligned with Entity id_ values.
+  std::vector<size_t> flux_;  // Aligned with MoleculeType id_ values.
   std::string name_;
   
   Biome(const std::string& name);
-  virtual std::string _str() const;
+  std::string _str() const;
 };
 
 Biome::Biome(const std::string& name): name_(name)
 {
-  flux_ = vector<size_t>(Entity::numEntities(), 0);
+  flux_ = vector<size_t>(MoleculeType::numMoleculeTypes(), 0);
 }
 
 std::string Biome::_str() const
@@ -95,17 +116,59 @@ std::string Biome::_str() const
   oss << "Biome \"" << name_ << "\"" << endl;
   return oss.str();
 }
-  
+
+
+// Contains the whole simulation model
+class Prokaryotic
+{
+public:
+  Prokaryotic();
+  std::vector<MoleculeType::Ptr> molecule_types_;
+  std::map<std::string, MoleculeType::Ptr> molecule_map_;
+
+  void addMoleculeType(MoleculeType::Ptr mt);
+  MoleculeType::Ptr molecule(const std::string& name) {
+    assert(molecule_map_.find(name) != molecule_map_.end());
+    return molecule_map_[name];
+  } 
+};
+
+Prokaryotic::Prokaryotic()
+{
+  // addMoleculeType(MoleculeType::Ptr(new MoleculeType("ATP", ":bang:", 503.15)));
+  addMoleculeType(MoleculeType::Ptr(new MoleculeType("ADP", ":briefcase:", 423.17)));
+  addMoleculeType(MoleculeType::Ptr(new MoleculeType("Phosphate", "P", 94.97)));
+  addMoleculeType(MoleculeType::Ptr(new MoleculeType("", "R", 1000)));
+  std::vector<MoleculeType::Ptr> constituents;
+  constituents.push_back(molecule("ADP"));
+  constituents.push_back(molecule("Phosphate"));
+  addMoleculeType(MoleculeType::Ptr(new MoleculeType("ATP", ":bang:", constituents)));
+
+  for (auto mt : molecule_types_)
+    cout << mt->str() << endl;
+}
+
+void Prokaryotic::addMoleculeType(MoleculeType::Ptr mt)
+{
+  molecule_types_.push_back(mt);
+  molecule_map_[mt->name_] = mt;
+}
+
+
 int main(int argc, char** argv)
 {
-  Entity e0("ATP", "bang");
-  Entity e1("ADP", "wallet");
-  //cout << e1.id() << endl;
+  // MoleculeType e0("ATP", "bang", 503.15);
+  // MoleculeType e1("ADP", "wallet", 423.17);
+  // MoleculeType e2("Phosphate", "", 94.97);
+  // //cout << e1.id() << endl;
 
-  cout << e0.str("  ") << endl;
+  // cout << e0.str() << endl;
 
-  Biome b0("Alkaline vents");
-  //cout << b0.flux_ << endl;
-  cout << b0.str() << endl;
+  // Biome b0("Alkaline vents");
+  // //cout << b0.flux_ << endl;
+  // cout << b0.str() << endl;
+
+  Prokaryotic prokaryotic;
+  
   return 0;
 }
