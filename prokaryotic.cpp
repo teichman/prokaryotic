ec09ce6ac0e62177387221f7aaad92a2aa691bca
Alex Teichman <teichman@cs.stanford.edu>
2022-11-29 22:47:47 -0800

    Adding some nicer printing.

--- prokaryotic.cpp
+++ prokaryotic.cpp
@@ -1,3 +1,4 @@
+#include <sstream>
 #include <vector>
 #include <iostream>
 #include <memory>
@@ -7,7 +8,30 @@ using std::shared_ptr;
 using std::cout, std::endl;
 using std::vector;
 
-class Entity
+// Just gives the prefix option for free.
+class Printable
+{
+public:
+  virtual ~Printable() {}
+
+  // Ugh maybe I should go back to python.
+  // https://stackoverflow.com/questions/37803255/how-to-call-base-classs-function-that-is-not-defined-in-derived-class
+  virtual std::string _str() const = 0;
+  virtual std::string str(const std::string& prefix="") const
+  {
+    std::istringstream iss(_str());
+    std::ostringstream oss;
+    for (std::string line; std::getline(iss, line); )
+      oss << prefix << line + "\n";
+
+    // Remove trailing newline.
+    std::string result = oss.str();
+    result.erase(std::remove(result.end() - 2, result.end(), '\n'), result.cend());
+    return result;
+  }
+};
+
+class Entity : public Printable
 {
 public:
   typedef std::shared_ptr<Entity> Ptr;
@@ -15,9 +39,10 @@ public:
   
   int id_;
   std::string name_;
+  std::string emoji_;
 
-  Entity(const std::string& name);
-
+  Entity(const std::string& name, const std::string& emoji);
+  virtual std::string _str() const;
   static size_t numEntities() { return num_entities_; }
   
 private:
@@ -26,15 +51,27 @@ private:
 
 size_t Entity::num_entities_ = 0;
 
-Entity::Entity(const std::string& name) : name_(name),
-                                          id_(num_entities_)
+Entity::Entity(const std::string& name,
+               const std::string& emoji) :
+  id_(num_entities_),
+  name_(name),
+  emoji_(emoji)
 {
   num_entities_ += 1;
   cout << "num_entities_: " << num_entities_ << endl;
   cout << "Created Entity with id " << id_ << endl;
 }
 
-class Biome
+std::string Entity::_str() const
+{
+  std::ostringstream oss;
+  oss << "Entity \"" << name_ << "\"" << endl;
+  oss << "id_: " << id_ << endl;
+  oss << "emoji_: " << emoji_ << endl;
+  return oss.str();
+}
+
+class Biome : public Printable
 {
 public:
   typedef std::shared_ptr<Biome> Ptr;
@@ -44,21 +81,31 @@ public:
   std::string name_;
   
   Biome(const std::string& name);
+  virtual std::string _str() const;
 };
 
 Biome::Biome(const std::string& name): name_(name)
 {
   flux_ = vector<size_t>(Entity::numEntities(), 0);
 }
-  
+
+std::string Biome::_str() const
+{
+  std::ostringstream oss;
+  oss << "Biome \"" << name_ << "\"" << endl;
+  return oss.str();
+}
   
 int main(int argc, char** argv)
 {
-  Entity e0("A");
-  Entity e1("B");
+  Entity e0("ATP", "bang");
+  Entity e1("ADP", "wallet");
   //cout << e1.id() << endl;
+
+  cout << e0.str("  ") << endl;
 
   Biome b0("Alkaline vents");
   //cout << b0.flux_ << endl;
+  cout << b0.str() << endl;
   return 0;
 }
