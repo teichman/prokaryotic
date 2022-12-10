0dccb251b55ef43912b40af19eaf715d983bf955
Alex Teichman <teichman@cs.stanford.edu>
2022-12-09 17:24:13 -0800

    Refactoring a bit to make a Comms class that Prokaryotic will use to have a clean api for broadcasting to clients.

--- scratch_zmq2python.cpp
+++ scratch_zmq2python.cpp
@@ -2,10 +2,9 @@
 #include <chrono>
 #include <thread>
 #include <iostream>
-#include <zmq_addon.hpp>
+#include <zmq.hpp>
 
 using namespace std;
-
 
 class MessageWrapper
 {
@@ -71,39 +70,50 @@ private:
     
 };
 
+class Comms
+{
+public:
+  zmq::context_t ctx_;
+  zmq::socket_t sock_;
+  
+  Comms() :
+    sock_(ctx_, zmq::socket_type::pub)
+  {
+    sock_.bind("tcp://127.0.0.1:53269");
+    string last_endpoint = sock_.get(zmq::sockopt::last_endpoint);
+    std::cout << "Connecting to " << last_endpoint << std::endl;
+  }
+
+  void broadcast(const MessageWrapper& msg)
+  {
+    sock_.send(msg, zmq::send_flags::none);
+  }
+};
 
 
 int main()
 {
-    zmq::context_t ctx;
-    zmq::socket_t sock1(ctx, zmq::socket_type::pub);
+  Comms comms;
+            
+  while (true) {
+    MessageWrapper mw;
+    Eigen::ArrayXXd mat = Eigen::ArrayXXd::Zero(2, 2);
+    mat(0, 0) = 1;
+    mat(0, 1) = 2;
+    mat(1, 0) = 3;
+    mat(1, 1) = 4;
+    mw.addField("matrix_something", mat);
+    Eigen::ArrayXd vec = Eigen::ArrayXd::Zero(4);
+    vec(0) = 13;
+    vec(3) = 42;
+    mw.addField("vector_something", vec);
+      
+    //zmq::message_t msg(mw);
+    cout << "Publishing msg of size " << mw.size() << endl;
+    comms.broadcast(mw);
+      
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  }
     
-    sock1.bind("tcp://127.0.0.1:53269");
-    const std::string last_endpoint =
-        sock1.get(zmq::sockopt::last_endpoint);
-    std::cout << "Connecting to "
-              << last_endpoint << std::endl;
-
-    while (true) {
-      
-      MessageWrapper mw;
-      Eigen::ArrayXXd mat = Eigen::ArrayXXd::Zero(2, 2);
-      mat(0, 0) = 1;
-      mat(0, 1) = 2;
-      mat(1, 0) = 3;
-      mat(1, 1) = 4;
-      mw.addField("matrix_something", mat);
-      Eigen::ArrayXd vec = Eigen::ArrayXd::Zero(4);
-      vec(0) = 13;
-      vec(3) = 42;
-      mw.addField("vector_something", vec);
-      
-      //zmq::message_t msg(mw);
-      cout << "Publishing msg of size " << mw.size() << endl;
-      sock1.send(mw, zmq::send_flags::none);
-      
-      std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-    }
-    
-    return 0;
+  return 0;
 }
